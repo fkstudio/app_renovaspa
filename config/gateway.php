f9646bc81e97fa63cca9c65c225b4dd965246810
dsalcedo <dsalcedo@epg.com.do>
2017-05-31 20:24:36 -0400

    cambios para produccion

--- config/gateway.php
+++ config/gateway.php
@@ -3,7 +3,7 @@
 return [
 	'url' => 'https://secure.networkmerchants.com/api/v2/three-step',
 	'api_key' => '2F822Rw39fx762MaV7Yy86jXGTC7sCDy',
-	'user' => 'demo',
-	'password' => 'password'
+	'user' => /*'demo',*/ 'renovaspa',
+	'password' => /*'password'*/ 'heath1098'
 ];
 
--- views/hotel/details.blade.php
+++ views/hotel/details.blade.php
@@ -3,6 +3,7 @@
 @section('title', $model->Name)
 
 @php
+    /*
     $countryName = strtolower(str_replace(' ', '-', $region->Country->Name));
     $regionName = strtolower(str_replace(' ', '-', $region->Name));
     $hotelName = str_replace(' ', '-', $model->Name);
@@ -17,6 +18,7 @@
     array_shift($files);
 
     $showActive = true;
+    */
 @endphp
 
 @section('content')
@@ -37,6 +39,98 @@
         </div>
     </div>
 </div>
+<div class="container-fluid">
+    @include('shared._breadcrumps')
+    <hr/>
+    <div class="row">
+        <div class="col-lg-6 col-md-6">
+            <!-- Top part of the slider -->
+            <div class="row">
+                <div class="col-lg-12 col-sm-12" id="carousel-bounding-box">
+                    <div class="carousel slide" id="hotel-carousel">        
+                        <!-- Carousel items -->
+                        <div class="carousel-inner">
+                            @php
+                                $countryName = strtolower(str_replace(' ', '-', $region->Country->Name));
+                                $regionName = strtolower(str_replace(' ', '-', $region->Name));
+                                $hotelName = str_replace(' ', '-', $model->Name);
+
+                                $folderPath = '/images/hotels/'.$countryName .'/'. $regionName .'/'.$hotelName;
+                                $photoPath = base_path().'/public'.$folderPath;
+                                $urlPath = URL::to('/').$folderPath;
+
+                                $files = scandir($photoPath);
+
+                                array_shift($files);
+                                array_shift($files);
+
+                                $showActive = true;
+                            @endphp
+                            <!-- Carousel items -->
+                            @foreach($files as $key => $file)
+                                @if(!strpos($file, 'thumbnail'))
+                                    @php
+                                      $active = ($showActive ? 'active' : '');
+                                    @endphp
+                                    <div  class="{{ $active }} item" data-slide-number="{{ $key }}">
+                                        <img src="{{ $urlPath . '/' . $file }}">
+                                    </div>
+                                    @php
+                                        $showActive = false;
+                                    @endphp
+                                @endif
+                            @endforeach
+                        </div>
+                        <!-- Carousel nav -->
+                        <a class="left carousel-control" href="#hotel-carousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </a>
+                        <a class="right carousel-control" href="#hotel-carousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row visible-lg">
+                <!-- Bottom switcher of slider -->
+                <ul class="hide-bullets">
+                    @foreach($files  as $key => $file)
+                        @if(strpos($file, 'thumbnail'))
+                            @php
+
+                            $active = ($key == 0 ? 'active' : '');
+
+                            @endphp
+                            <li class="col-lg-4 col-sm-3 col-xs-4">
+                                <a style="background: url({{ $urlPath .'/'. $file }});background-size: cover;background-position: center center;" class="thumbnail thumbnail-carousel" id="carousel-selector-{{ $key }}">
+                                    <!-- <img src=""> -->
+                                </a>
+                            </li>
+                        @endif
+                    @endforeach
+                </ul>   
+            </div>
+        </div>
+        <div class="clearfix hidden-lg hidden-md"></div>
+        <br class="hidden-lg hidden-md" />
+        <div class="col-lg-6 col-md-6">
+            <h2 class="details-hotel-title">{{ $model->Name }}</h2>
+            <div class="clearfix"></div>
+            <div class="under-title-line"></div>
+            <p><strong>{{ trans("hotel.address") }}</strong>: {!! $model->Address !!}<br/>
+               <strong>{{ trans("hotel.hours") }}</strong>: {{ $model->OpenAt->format('H:i:s a') }} - {{ $model->ClosetAt->format('H:i:s a') }}</p>
+            @if (empty($model->Description))
+            <p>No description to show</p>
+            @else
+            <span style="font-size: 12px;">
+            {!! $model->Description !!}
+            </span>
+            @endif
+        </div>
+    </div>
+</div>  
+<?php
+/* 
 <div class="container-fluid">
 	@include('shared._breadcrumps')
 	<hr/>
@@ -98,7 +192,9 @@
 	    	@endif
 	    </div>
 	</div>
-</div>	
+</div>
+ */
+?>	
 <div class="clearfix"></div>
 <br/>
 <br/>
